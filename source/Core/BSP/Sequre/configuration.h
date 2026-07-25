8f60c15814b44ad14c5b84d66cda643957b7ce20
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2026-07-25 19:28:24 +0500

    update soldering ui

--- source/Core/BSP/Sequre/configuration.h
+++ source/Core/BSP/Sequre/configuration.h
@@ -96,7 +96,7 @@
  * Detailed soldering screen
  * Detailed idle screen (off for first time users)
  */
-#define DETAILED_SOLDERING 1 // 0: Disable 1: Enable - Default 0
+#define DETAILED_SOLDERING 0 // 0: Disable 1: Enable - Default 0
 #define DETAILED_IDLE      0 // 0: Disable 1: Enable - Default 0
 
 #define CUT_OUT_SETTING          0  // default to no cut-off voltage
@@ -295,7 +295,7 @@
 #define MIN_CALIBRATION_OFFSET 100 // Min value for calibration
 #endif
 #ifndef SOLDERING_TEMP
-#define SOLDERING_TEMP 320 // Default soldering temp is 320.0 °C
+#define SOLDERING_TEMP 40 // Default soldering temp is 320.0 °C
 #endif
 #ifndef PID_TIM_HZ
 #define PID_TIM_HZ (8) // Tick rate of the PID loop
--- source/Core/Threads/UI/drawing/mono_128x32/draw_soldering_basic_status.cpp
+++ source/Core/Threads/UI/drawing/mono_128x32/draw_soldering_basic_status.cpp
@@ -1,45 +1,49 @@
 #include "power.hpp"
+#include "SolderingCommon.h"
 #include "ui_drawing.hpp"
 #ifdef OLED_128x32
 
-void ui_draw_soldering_basic_status(bool boostModeOn) {
-  OLED::setCursor(0, 0);
-  // We switch the layout direction depending on the orientation of the oled
-  if (OLED::getRotation()) {
-    // battery
-    ui_draw_power_source_icon();
-    // Space out gap between battery <-> temp
-    OLED::print(LargeSymbolSpace, FontStyle::LARGE);
-    // Draw current tip temp
-    ui_draw_tip_temperature(true, FontStyle::LARGE);
-
-    // We draw boost arrow if boosting,
-    // or else gap temp <-> heat indicator
-    if (boostModeOn) {
-      OLED::drawSymbol(2);
-    } else {
-      OLED::print(LargeSymbolSpace, FontStyle::LARGE);
-    }
-
-    // Draw heating/cooling symbols
-    OLED::drawHeatSymbol(X10WattsToPWM(x10WattHistory.average()));
+static void printWatts(void) {
+  uint32_t x10Watt = x10WattHistory.average();
+  if (x10Watt > 999) {
+    OLED::print(SmallSymbolSpace, FontStyle::SMALL);
+    OLED::printNumber(x10WattHistory.average() / 10, 3, FontStyle::SMALL);
   } else {
-    // Draw heating/cooling symbols
-    OLED::drawHeatSymbol(X10WattsToPWM(x10WattHistory.average()));
-    // We draw boost arrow if boosting,
-    // or else gap temp <-> heat indicator
-    if (boostModeOn) {
-      OLED::drawSymbol(2);
-    } else {
-      OLED::print(LargeSymbolSpace, FontStyle::LARGE);
-    }
-    // Draw current tip temp
-    ui_draw_tip_temperature(true, FontStyle::LARGE);
-    // Space out gap between battery <-> temp
-    OLED::print(LargeSymbolSpace, FontStyle::LARGE);
-
-    ui_draw_power_source_icon();
+    OLED::printNumber(x10WattHistory.average() / 10, 2, FontStyle::SMALL);
+    OLED::print(SmallSymbolDot, FontStyle::SMALL);
+    OLED::printNumber(x10WattHistory.average() % 10, 1, FontStyle::SMALL);
   }
+  OLED::print(SmallSymbolWatts, FontStyle::SMALL);
+}
+
+void ui_draw_soldering_basic_status(bool boostModeOn) {
+  uint8_t midX, voltX;
+  if (OLED::getRotation()) {
+    midX = 12; voltX = 0;
+  } else {
+    midX = 74; voltX = 116;
+  }
+
+  OLED::setCursor(0, 8);
+  OLED::printNumber(getTipTemp(), 3, FontStyle::LARGE);
+  OLED::printSymbolDeg(FontStyle::EXTRAS);
+
+  OLED::setCursor(midX, 0);
+  OLED::printNumber(getSettingValue(SettingsOptions::SolderingTemp), 3, FontStyle::SMALL);
+  OLED::printSymbolDeg(FontStyle::SMALL);
+  // OLED::print(SmallSymbolSpace, FontStyle::SMALL);
+  if (boostModeOn) OLED::drawSymbol(2);
+
+  OLED::setCursor(midX, 8);
+  printWatts();
+
+  OLED::setCursor(midX, 16);
+  OLED::drawSymbol(14);
+  uint8_t heat = X10WattsToPWM(x10WattHistory.average()) / 31;
+  OLED::drawFilledRect(midX, 16, midX + 12, 16 + 2 + (8 - heat), true);
+
+  OLED::setCursor(voltX, 0);
+  ui_draw_power_source_icon();
 }
 
 #endif
